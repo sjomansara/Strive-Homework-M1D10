1dc9acdec61beb6d661f83503025adb808a2341f
sjomansara <sara.sjoman@hotmail.com>
2021-07-02 14:19:51 +0200

    solved ex22

---
+++ index.js
@@ -0,0 +1,28 @@
+//JS EXERCISES
+
+      //  21) Given variable x = "John" and y = "Doe", write on the console log "John <> Doe"
+
+      /* let x = "John"
+      let y = "Doe"
+      
+      console.log(John <> Doe) */
+
+      //  22) Create an object with properties such name, surname, email
+
+     const person = {
+         name: "Sara",
+         surname: "Sjöman",
+         email: "sara.sjoman@hotmail.com"
+     }
+
+     console.log(person)
+
+
+      //  23) Delete Email from the previously created object
+      //  24) Create an array with 10 strings in it
+      //  25) Print in the console every string in the previous array
+      //  26) Create an array with 100 random numbers in it
+      //  27) Wrote a function to get the MAX and the MIN from the previously created array
+      //  28) Create an array of arrays, in which every array has 10 random numbers
+      //  29) Create a function that gets 2 arrays and returns the longest one
+      //  30) Create a function that gets 2 arrays of numbers and returns the one with the higher sum of values
