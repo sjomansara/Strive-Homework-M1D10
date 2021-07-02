471136dc724144a458e48e14b28f1085ba1bab0c
sjomansara <sara.sjoman@hotmail.com>
2021-07-02 17:18:47 +0200

    removed comments

--- index.js
+++ index.js
@@ -96,15 +96,3 @@
 
       console.log(returnHigherSum([1, 2, 3, 4], [2, 3, 4, 5]))
 
-// DOM 
-
-// 31) Get element with ID "container" from the page
-// 32) Get every "td" from the page
-// 33) Create a cycle that prints the text inside every td of the page
-// 34) Write a function to change the heading of the page
-// 35) Write a function to add an extra row to the table
-// 36) Write a function to add the class "test" to each row in the table
-// 37) Write a function to add a red background to every link in the page
-// 38) Console log "Page loaded" when the page is correctly loaded
-// 39) Write a function to add new items to a UL
-// 40) Write a function to empty a list
